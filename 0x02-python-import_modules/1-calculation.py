b4d89443d42b397c576d50f0a72a41f0d1f7695a
MrSavvy1 <destydesty112@gmail.com>
2024-01-04 12:26:56 -0800

    realab

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import calculator_1
+def main():
+    a = 10
+    b = 5
+    summation = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, summation))
+    print("{:d} - {:d} = {:d}".format(a, b, sub))
+    print("{:d} * {:d} = {:d}".format(a, b, mul))
+    print("{:d} / {:d} = {:d}".format(a, b, div))
+
+if __name__ == '__main__':
+    main()
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+Experimenting on some python-import-modules
